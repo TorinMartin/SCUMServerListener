3c4778727639d8d8c2d73fce843e4ed549bd70b0
Torin Martin <w0404679@nscc.ca>
2020-06-28 12:51:11 -0300

    Server ID Lookup using JSON rather than regex

--- ServerData.cs
+++ ServerData.cs
@@ -47,14 +47,14 @@ namespace ServerListener
                 }
             }
 
-            return "https://www.battlemetrics.com/servers/scum?q=" + serverString + "&sort=score";
+            //return "https://www.battlemetrics.com/servers/scum?q=" + serverString + "&sort=score";
+            return "https://api.battlemetrics.com/servers?sort=rank&fields%5Bserver%5D=rank%2Cname%2Cplayers%2CmaxPlayers%2Caddress%2Cip%2Cport%2Ccountry%2Clocation%2Cdetails%2Cstatus&relations%5Bserver%5D=game%2CserverGroup&filter%5Bgame%5D=scum&filter%5Bsearch%5D=" + serverString;
         }
 
         public List<Server> GetServerID(string lookUpString)
         {
 
-            string htmlCode;
-            string data = null;
+            string json = null;
 
             List<Server> results = new List<Server>();
 
@@ -62,28 +62,39 @@ namespace ServerListener
             {
                 try
                 {
-                    htmlCode = client.DownloadString(lookUpString);
+                    json = client.DownloadString(lookUpString);
 
-                    Regex serverIdReg = new Regex("{.*}}</script>");
-                    Match serverIdMatch = serverIdReg.Match(htmlCode);
+                    dynamic dataSet = JObject.Parse(json);
 
-                    if (serverIdMatch.Success)
-                    {
-                        var result = serverIdMatch.ToString();
-                        if (result.Contains("</script>"))
-                        {
-                            data = result.Remove(result.IndexOf("</script>"), "</script>".Length);
-                        }
-                    }
-
-                    dynamic dataSet = JObject.Parse(data);
-                    var servers = (JObject)dataSet["servers"]["servers"];
+                    var servers = dataSet["data"];
 
                     foreach (var server in servers)
                     {
-                        results.Add(new Server(server.Key, server.Value.Value<string>("name")));
+                        results.Add(new Server((string)server["id"], (string)server["attributes"]["name"]));
                     }
                     return results;
+
+                    //** Grabs JSON from webpage and removes the script tags prior to parsing it. Old method and not used currently **//
+                    //Regex serverIdReg = new Regex("{.*}}</script>");
+                    //Match serverIdMatch = serverIdReg.Match(htmlCode);
+
+                    //if (serverIdMatch.Success)
+                    //{
+                    //    var result = serverIdMatch.ToString();
+                    //    if (result.Contains("</script>"))
+                    //    {
+                    //        data = result.Remove(result.IndexOf("</script>"), "</script>".Length);
+                    //    }
+                    //}
+
+                    //dynamic dataSet = JObject.Parse(data);
+                    //var servers = (JObject)dataSet["servers"]["servers"];
+
+                    //foreach (var server in servers)
+                    //{
+                    //    results.Add(new Server(server.Key, server.Value.Value<string>("name")));
+                    //}
+                    //return results;
                 }
                 catch (WebException)
                 {
