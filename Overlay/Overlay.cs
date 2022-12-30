7d8e8b228a7f61a6e8f343f435f8bd8dece46358
TorinMartin <alowsedan@gmail.com>
2022-12-29 22:06:44 -0400

    Remove debug console output

--- Overlay/Overlay.cs
+++ Overlay/Overlay.cs
@@ -237,7 +237,6 @@ namespace SCUMServerListener
 					GetCursorPos(out mouse);
 					this.X = mouse.X;
 					this.Y = mouse.Y;
-					Console.WriteLine(GetAsyncKeyState(Keys.LButton).ToString());
 					if (GetAsyncKeyState(Keys.LButton) > 0) {
 						break;
 					}
